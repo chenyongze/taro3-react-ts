45c9b3eee498536e0e894f948747d356e03d4964
chenyongze <sapphire.php@gmail.com>
2020-07-11 08:22:56 +0800

    start

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,122 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { View, Button, Text } from '@tarojs/components'
+
+import { add, minus, asyncAdd } from '../../actions/counter'
+
+// import { ClButton } from 'mp-colorui'
+import { AtButton, AtAvatar, AtGrid, AtCalendar } from 'taro-ui'
+
+import './index.scss'
+
+// #region 书写注意
+//
+// 目前 typescript 版本还无法在装饰器模式下将 Props 注入到 Taro.Component 中的 props 属性
+// 需要显示声明 connect 的参数类型并通过 interface 的方式指定 Taro.Component 子类的 props
+// 这样才能完成类型检查和 IDE 的自动提示
+// 使用函数模式则无此限制
+// ref: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/20796
+//
+// #endregion
+
+type PageStateProps = {
+  counter: {
+    num: number
+  }
+}
+
+type PageDispatchProps = {
+  add: () => void
+  dec: () => void
+  asyncAdd: () => any
+}
+
+type PageOwnProps = {}
+
+type PageState = {}
+
+type IProps = PageStateProps & PageDispatchProps & PageOwnProps
+
+interface Index {
+  props: IProps;
+}
+
+@connect(({ counter }) => ({
+  counter
+}), (dispatch) => ({
+  add() {
+    dispatch(add())
+  },
+  dec() {
+    dispatch(minus())
+  },
+  asyncAdd() {
+    dispatch(asyncAdd())
+  }
+}))
+class Index extends Component {
+  componentWillReceiveProps(nextProps) {
+    console.log(this.props, nextProps)
+  }
+
+  componentWillUnmount() { }
+
+  componentDidShow() { }
+
+  componentDidHide() { }
+
+  render() {
+    return (
+      <View className='index'>
+        <Button className='add_btn' onClick={this.props.add}>+</Button>
+        <Button className='dec_btn' onClick={this.props.dec}>-</Button>
+        <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
+
+        <AtButton >按钮文案</AtButton>
+
+        <AtAvatar image='https://jdc.jd.com/img/200'></AtAvatar>
+        <AtAvatar text='凹凸实验室'></AtAvatar>
+        <AtAvatar circle image='https://jdc.jd.com/img/200'></AtAvatar>
+        <AtAvatar circle text='凹凸实验室'></AtAvatar>
+
+
+        <View><Text>{this.props.counter.num}</Text></View>
+        <View><Text>Hello, World</Text></View>
+
+        <AtCalendar minDate="2018/1/1" maxDate="2018/10/1" />
+
+        <AtGrid data={
+          [
+            {
+              image: 'https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png',
+              value: '领取中心'
+            },
+            {
+              image: 'https://img20.360buyimg.com/jdphoto/s72x72_jfs/t15151/308/1012305375/2300/536ee6ef/5a411466N040a074b.png',
+              value: '找折扣'
+            },
+            {
+              image: 'https://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png',
+              value: '领会员'
+            },
+            {
+              image: 'https://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png',
+              value: '新品首发'
+            },
+            {
+              image: 'https://img14.360buyimg.com/jdphoto/s72x72_jfs/t17251/336/1311038817/3177/72595a07/5ac44618Na1db7b09.png',
+              value: '领京豆'
+            },
+            {
+              image: 'https://img30.360buyimg.com/jdphoto/s72x72_jfs/t5770/97/5184449507/2423/294d5f95/595c3b4dNbc6bc95d.png',
+              value: '手机馆'
+            }
+          ]
+        } />
+      </View>
+    )
+  }
+}
+
+export default Index
+
